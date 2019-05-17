89ebbf0e37b89527bef06bd1123613d307d78a98
Pavel Altukhov <ai.centaur@gmail.com>
2019-05-17 12:27:43 -0700

    logic.js file update 1.4

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,18 +12,15 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(map);
 
+// Function that will determine the color of a earthquake location based on the magnitude it belongs to
 function chooseColor(mag) {
-    switch (mag) {
-    case (mag > 5): return "yellow";
-    case (mag > 4): return "red";
-    case (mag > 3): return "orange";
-    case (mag > 2): return "green";
-    case (mag > 1): return "purple";
-    default: return "red";
-    }
-  }
-
-  console.log("switch function works")
+  if (mag > 5) {return "#f44242"}
+  else if (mag > 4) {return "#f49241"}
+  else if (mag > 3) {return "#f4ee41"}
+  else if (mag > 2) {return "#4141f4"}
+  else if (mag > 1) {return "#f441e2"}
+  else {return "#73f441"}
+}
 
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
@@ -33,11 +30,39 @@ d3.json(link).then(function(data) {
     L.geoJson(data, {
       style: function(feature) {
         return {
-          color: "white",
           fillColor: chooseColor(feature.properties.mag),
           fillOpacity: 0.5,
           weight: 0.5
         };
+      },
+// Create a circle symbol to use with a GeoJSON layer instead of the default blue marker
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+
+      onEachFeature: function(feature, layer) {
+        // Set mouse events to change map styling
+        layer.on({
+          // When a user's mouse touches a map feature, the mouseover event calls this function; that feature's opacity changes to 90% so that it stands out
+          mouseover: function(event) {
+            layer = event.target;
+            layer.setStyle({
+              fillOpacity: 1.5
+            });
+          },
+          // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+          mouseout: function(event) {
+            layer = event.target;
+            layer.setStyle({
+              fillOpacity: 0.5
+            });
+          },
+        });
+        // Giving each feature a pop-up with information pertinent to it
+        layer.bindPopup(`<h4>MAGNITUDE:</h4> ${feature.properties.mag}<hr>
+                        <h4>LOCATION:</h4> ${feature.properties.place}`)
       }
     }).addTo(map);
   });
+
+  
